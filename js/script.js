ae18a94ab4017ad3d6c9c503b73806c327bafa88
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-07-18 15:18:18 +0200

    collegato container in js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,2 @@
-// Commento singola riga
-
-/*
-
-    Commento multipla riga
-
-*/
+const imagesContainer = document.querySelector('.images-container');
+console.log('imagesContainer', imagesContainer, typeof imagesContainer);
