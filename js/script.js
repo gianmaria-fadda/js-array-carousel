cced73e3f6a97a6576a52c011de816b1a5946c5d
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-07-18 15:46:00 +0200

    creata parte js e aggiunto div buttons in html e css

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
 const imagesContainer = document.querySelector('.images-container');
 console.log('imagesContainer', imagesContainer, typeof imagesContainer);
+
+const images = [
+    'img/01.webp',
+    'img/02.webp',
+    'img/03.webp',
+    'img/04.webp',
+    'img/05.webp'
+];
+
+console.log(images);
+console.log(images[0]);
+console.log(images[1]);
+console.log(images[2]);
+console.log(images[3]);
+console.log(images[4]);
+
+for (let i = 0; i < images.length; i++) {
+    if (i == 0) {
+        imagesContainer.innerHTML += `<img src="${images[i]}" class="active">`
+    }
+    else {
+        imagesContainer.innerHTML += `<img src="${images[i]}">`
+    }
+}
